99b3a93b450fdc51675c41e9c7fb42dd3bf13fbc
VSRohod <89645335+VSRohod@users.noreply.github.com>
2024-09-16 11:20:24 -0300

    v0.01
    Documentando o projeto com comentários e criando as funcionalidades de colaboradores e areas de código para que o usuário possa coletar as informações diretamente

---
+++ scripts/descricoesCodes.js
@@ -0,0 +1,13 @@
+// documento onde colocamos as descrições técnicas a serem inseridas em suas respectivas textarea selecionadas no documento Scripts
+// Colocados aqui para facilitar a inserção de comandos como string sem que o html interprete as informações
+const htmlBase = `
+<!DOCTYPE html>
+    <html lang='en'>
+        <head>
+            <meta charset='UTF-8'>
+            <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+            <title>Document</title>
+        </head>
+        <body>
+        </body>
+    </html>`
