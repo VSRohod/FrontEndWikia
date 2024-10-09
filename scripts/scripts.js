48776120549dcf007ef0c714d94d2cb28ac0c9fa
VSRohod <89645335+VSRohod@users.noreply.github.com>
2024-10-09 11:45:04 -0300

    v0.02
    Reestrurando o site e adicionando o conteudo de localStorage JS

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,33 +1,29 @@
 // Código de Js para que as caixas de texto façam a alteração de altura dinamicamente de acordo com seu conteudo
 let areaDeTexto = document.querySelectorAll("textarea")
-        
+
 window.addEventListener("DOMContentLoaded", () => {
     areaDeTexto.forEach((elemento) => {
-    elemento.style.height = `${elemento.scrollHeight}px`
-  })
-})    
-
-
-// Seleção de cada caixa de texto do site e qual descrição do documento descricoesCodes eles vão receber, todos em const para garantir que os valores não sejam alterados de acordo com o tempo
-$("#htmlBase").val(htmlBase)
+        elemento.style.height = `${elemento.scrollHeight}px`
+    })
+})
 
 // Código para imprimir dinamicamente os responsáveis pelo projeto apenas adicionando as informações no array abaixo para logo em seguida aparecer na tela
 // Lista de colaboradores, cada um como objeto e com seus respectivos atributos: Nome, link de github, icon de conta do Github
 const colaboradores = [
     {
-        nome: "VSRohod" ,
-        github: "https://github.com/VSRohod" ,
-        icon: "https://avatars.githubusercontent.com/u/89645335?v=4" 
+        nome: "VSRohod",
+        github: "https://github.com/VSRohod",
+        icon: "https://avatars.githubusercontent.com/u/89645335?v=4"
     },
     {
-        nome: "PedroGabrielsds" ,
-        github: "https://github.com/PedroGabrielsds" ,
-        icon: "https://avatars.githubusercontent.com/u/177778503?v=4" 
+        nome: "PedroGabrielsds",
+        github: "https://github.com/PedroGabrielsds",
+        icon: "https://avatars.githubusercontent.com/u/177778503?v=4"
     }
 ]
 
 // repetição para dinamicamente ver os colaboradores da lista e  criar um card para cada um com suas respectivas informações
-colaboradores.forEach((colaborador)=>{
+colaboradores.forEach((colaborador) => {
     document.getElementById("colaboradoresLista").innerHTML += `
     <a href="${colaborador.github}">
         <div class="card border-0" style="width: 6rem;">
